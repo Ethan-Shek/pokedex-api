6d00f2d8d158767471981077cee11e5711d7dc9a
Ethan-Shek <dinoeshek@gmail.com>
2025-10-15 11:45:00 -0400

    Update server.js removed const port 3000

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import http from 'http';
 import { handleHTML } from './responses/htmlResponses.js';
 import { handleJSON } from './responses/jsonResponses.js';
 
-const PORT = 3000;
-
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
 
